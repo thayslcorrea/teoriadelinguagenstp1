1de544b0923ad863d1d1da80ad12693bf21f049e
Thays Lacerda <thays.lacerdac@gmail.com>
2021-03-28 11:35:41 -0300

    Código fonte

---
+++ index.php
@@ -0,0 +1,100 @@
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Teoria de Linguagens</title>
+</head>
+
+<body style="width: 100%;">
+    <div class="flex-md-row align-items-center px-md-4 mb-3 bg-white border-bottom shadow-sm" style=" display: flex;">
+        <h5><img src="imgs/ufsj.png" style="width: 200px; padding: 5%;" alt="Mais Controle"></h5>
+        <div style="width: 100%;">
+            <nav class="nav justify-content-end">
+                <b>Trabalho Prático I - Teoria de Linguagens</b>
+            </nav>
+            <nav class="nav justify-content-end">
+                <b>Thays L. Corrêa</b>
+            </nav>
+        </div>
+    </div>
+
+    <div class="container d-flex" style="float: left; width: 40%; margin-left: 3%;">
+        <div class="card mt-4" style="width: 95%;">
+            <div class="card-header text-center">
+                <h4>Palavras Processadas</h4>
+            </div>
+
+            <div class="card-body">
+                <div class="form-group d-flex">
+                    <div class=" col-md-8">
+                        <input class="form-control input-search" type="text" id="palavra" placeholder="Digite uma palavra" /><br>
+
+                        <input type="file" id="arquivo" name="arquivo" accept=".txt, .doc, .docx">
+                    </div>
+
+                    <div class="col-md-4 text-right">
+                        <button type="submit" class="btn btn-lg btn-confirmar">
+                            <span style="font: normal normal 600 15px/20px Segoe UI;letter-spacing: 0.85px;color: #FFFFFF;">
+                                Verificar
+                            </span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-body">
+                <div class="form-group col-md-12" style="max-height: 580; overflow-y: auto;">
+                    <table class="table table-bordered table-palavras">
+                        <thead>
+                            <th style="text-align: center;">Palavra</th>
+                            <th style="text-align: center;" colspan="2">Status</th>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container" style="float: right; width: 54%; margin-right: 3%; margin-bottom: 5%;">
+        <div class="card mt-4" style="width: 100%;">
+            <div class="card-header text-center">
+                <h4>Autômatos</h4>
+                <!-- <h4>ER = 5(0t + 4h)+7</h4> -->
+            </div>
+
+            <div class="card-body">
+                <h3>AFNε:</h3>
+                <img src="imgs/AFNe.jpg" />
+
+                <h3>AFN:</h3>
+                <img src="imgs/AFN.png" height="180"/>
+
+                <h3>AFD:</h3>
+                <img src="imgs/AFD2.png" height="160" />
+            </div>
+        </div>
+    </div>
+
+</body>
+
+
+<!-- Importando o jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+<!-- Importando o js do bootstrap -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<script src="script.js"></script>
+
+</html>
---
+++ script.js
@@ -0,0 +1,207 @@
+var flag = false, mensagem = '', count = 0;
+
+// Função responsável por capturar o click do botão VERIFICAR
+$(document).on('click', '.btn-confirmar', function () {
+    var palavra = document.getElementById("palavra").value;
+
+    // Verifica se a palavra digitada no campo é igual a vazio
+    if (palavra == "") {
+        // Se sim, faz a leitura do arquivo
+        loadFileAsText()
+    } else {
+        // Se não, faz o processamento desta palavra e reseta o campo de entrada
+        document.getElementById("palavra").value = "";
+        verifica(palavra, "campo")
+    }
+})
+
+// Função responsável por ler os arquivos .txt
+function loadFileAsText() {
+    var fileToLoad = document.getElementById("arquivo").files[0];  //a intenção é não precisar de botões pra escolher arquivo e nem para
+    var fileReader = new FileReader();                                //executar o script, tudo deve acontecer quando cricar para abrir a aplicação
+    fileReader.onload = function (fileLoadedEvent) {
+        var textFromFileLoaded = fileLoadedEvent.target.result;
+        var texto = textFromFileLoaded;
+        listar(texto);
+    };
+
+    fileReader.readAsText(fileToLoad, "UTF-8");
+}
+
+// Função responsável pela leitura individual de cada linha
+function listar(texto) {
+    var itens = texto.split("\n"); // define que linhas devem ser consultadas
+
+    for (var i = 0; i < itens.length; i++) {
+        var valores = itens[i].split("\t");// espaços TAB definem colunas que serão consultadas
+
+        // Verifica apenas palavras diferentes de vazio, uma vez que a linguagem não permite
+        if (valores[6 & 1] != "") {
+            // Ao ler cada palavra, a mesma é enviada para função VERIFICA que irá processá-la no autômato
+            verifica(valores[6 & 1], "arquivo");
+        }
+
+    }
+}
+
+// Função responsável pelo loop (4h + 0t)
+function loop(palavra, indice) {
+
+    // Verifica se a palavra em um determinado indice (i) é = 0
+    if (palavra.substring(indice, indice + 1) === "0") {
+        // Se sim, atualizamos a variável de controle dizendo que conseguimos ler/aceitar mais um caractere
+        count++;
+
+        // Verifica se a palavra no próximo indíce (i+1) é = t
+        if (palavra.substring(indice + 1, indice + 2) === "t") {
+
+            // Se sim, o par de caracteres foi aceito e ambos processados 
+            count++;
+            return true;
+        } else {
+
+            // Se não, a palavra é rejeitada por ter lido qualquer caractere diferente de t após ter lido o 0
+            mensagem = '<b style="color:black;">' + palavra.substring(0, indice + 1) + '</b><b style="color:red;">' + palavra.substring(indice + 1, indice + 2) + '</b><b style="color:black;">' + palavra.substring(indice + 2, palavra.length) + '</b>';
+            return false;
+        }
+
+        // Verifica se a palavra em um determinado indice (i) é = 4
+    } else if (palavra.substring(indice, indice + 1) === "4") {
+        // Se sim, atualizamos a variável de controle dizendo que conseguimos ler/aceitar mais um caractere
+        count++;
+
+        // Verifica se a palavra no próximo indíce (i+1) é = h
+        if (palavra.substring(indice + 1, indice + 2) === "h") {
+
+            // Se sim, o par de caracteres foi aceito e ambos processados 
+            count++;
+            return true;
+        } else {
+
+            // Se não, a palavra é rejeitada por ter lido qualquer caractere diferente de h após ter lido o 4
+            mensagem = '<b style="color:black;">' + palavra.substring(0, indice + 1) + '</b><b style="color:red;">' + palavra.substring(indice + 1, indice + 2) + '</b><b style="color:black;">' + palavra.substring(indice + 2, palavra.length) + '</b>';
+            return false;
+        }
+
+    } else {
+        // Caso o caractere no indíce (i) não seja = a 0 ou 4, a palavra é rejeitada por isso
+        mensagem = '<b style="color:black;">' + palavra.substring(0, indice) + '</b><b style="color:red;">' + palavra.substring(indice, indice + 1) + '</b><b style="color:black;">' + palavra.substring(indice + 1, palavra.length) + '</b>';
+        return false;
+    }
+}
+
+function verifica(palavra, origem) {
+
+    // Variável de controle para verificar aceitação/rejeição de cada palavra
+    flag = false;
+
+    // Variável responsável por exibir em que momento a palavra foi rejeitada
+    mensagem = '';
+
+    // Variável para controlar se toda a palavra foi percorrida
+    count = 0;
+
+    var tamanho_palavra = palavra.length;
+
+    // Verificação para retirar 1 caractere, referente a quebra de linha, do tamanho da palavra para todas as palavras que são vindas do arquivo.
+    if (origem == "arquivo") {
+        tamanho_palavra -= 1;
+    }
+
+    // Verifica se a palavra começa com 5
+    if (palavra.substring(0, 1) == "5") {
+        var i = 1;
+
+        // Se sim, então passamos para o próximo caractere
+        count++;
+
+        // A função do while garante que o loop de (4h ou 0t) irá ocorrer pelo menos 1 vez
+        do {
+            // Função responsável por controlar o loop
+            flag = loop(palavra, i);
+
+            // Verificação se o conjunto de 2 caracteres foi aceito ou rejeitado. 
+            if (!flag) {
+                // Se foi rejeitado, ou seja, chegou combinação diferente de 4h ou 0t, então encerramos o laço e rejeitamos a palavra pela flag = false
+                i = tamanho_palavra;
+            } else {
+                // Se foi aceito, o laço continua até atingir o tamanho da palavra ou encontrar pares de caracteres diferentes dos definidos na linguagem
+                i += 2;
+            }
+
+        } while (i < tamanho_palavra - 2);
+
+        // console.log(palavra)
+        // console.log(count)
+        // console.log(tamanho_palavra)
+        // console.log(flag)
+
+
+        // Verificação para ver se a palavra foi aceita até aqui, ou seja: começa com 5 e repete 1 ou n vezes de 4h ou 0t
+        if (flag) {
+            // Verifica se está no último caractere da palavra
+            if (count + 1 == tamanho_palavra) {
+                // E se este é igual a 7
+                if (palavra.substring(tamanho_palavra - 1, tamanho_palavra) == "7") {
+                    console.log("aqui");
+                    count++;
+                    // Se sim, terminamos de processar a palavra e ela foi aceita
+                    flag = true;
+                } else {
+                    // Se não, a palavra é rejeitada por não terminar com 7
+                    mensagem = '<b style="color:black;">' + palavra.substring(0, tamanho_palavra - 1) + '</b><b style="color:red;">' + palavra.substring(tamanho_palavra - 1, tamanho_palavra) + '</b>';
+                    flag = false;
+                }
+            } else {
+               
+                // A palavra é rejeitada por ter caracteres após o fragmento que foi aceito. Ex.: 54h77 - fragmento aceito: 54h7
+                if(palavra.substring(count, count + 1) == "7" && palavra.substring(count, count + 1) != ""){
+                    mensagem = '<b style="color:black;">' + palavra.substring(0, tamanho_palavra - 2) + '</b><b style="color:red;">' + palavra.substring(tamanho_palavra - 2, tamanho_palavra - 1) + '</b><b style="color:black;">' + palavra.substring(tamanho_palavra - 1, tamanho_palavra) + '</b>';
+                }else{
+                    if (palavra.substring(count, count + 1) != "") {
+                        // A palavra é rejeitada por não terminar no estado final
+                        mensagem = '<b style="color:black;">' + palavra.substring(0, tamanho_palavra) + '</b><b style="color:red;">?</b>';
+
+                    } else {
+                        // A palavra é rejeitada por encontrar algum caracter entre as repetições de (4h + 0t) e o 7
+                        mensagem = '<b style="color:black;">' + palavra.substring(0, tamanho_palavra - 2) + '</b><b style="color:red;">' + palavra.substring(tamanho_palavra - 2, tamanho_palavra - 1) + '</b><b style="color:black;">' + palavra.substring(tamanho_palavra - 1, tamanho_palavra) + '</b>';
+                    }
+                }
+
+                
+                flag = false;
+            }
+        }
+
+    } else {
+        // Se a palavra não começar com 5 ela é rejeitada no primeiro caractere
+        mensagem = '<b style="color:red;">' + palavra.substring(0, 1) + '</b><b style="color:black;">' + palavra.substring(1, palavra.length) + '</b>';
+        flag = false;
+    }
+
+    // Verifica se a palavra foi aceita/rejeitada e insere na tabela
+    if (flag) {
+        insereTable(palavra, "ACEITA");
+    } else {
+        insereTable(palavra, "REJEITADA");
+    }
+}
+
+function insereTable(palavra, status) {
+
+    var tr = '<tr><td>' + palavra + '</td>';
+
+    // Caso a palavra tenha sido rejeitada, inserimos uma coluna (mensagem) para mostrar em que momento ela foi rejeitada 
+    if (status == 'REJEITADA') {
+        tr += '<td style="color: red;">' + status + '</td>';
+        tr += '<td style="color: red;">' + mensagem + '</td>';
+    } else {
+        // Caso contrário, insere apenas a palavra e o seu status de aceitação
+        tr += '<td style="color: green;">' + status + '</td><td> - </td>';
+    }
+
+    tr += '</tr>';
+
+    $('.table-palavras > tbody').append(tr);
+
+}
